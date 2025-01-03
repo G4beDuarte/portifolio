4be93f35e58af939545a9d448e6d11e914af7c11
Gabriel Duarte <gabriel.anpedu@gmail.com>
2025-01-03 15:27:00 -0300

    feat: adds onClick at StyledButton

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -11,11 +11,17 @@ const Hero = () => {
         backgroundColor: theme.palette.primary.main,
         height: "100vh",
         display: "flex",
-        alignItems: "center"
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]:{ // <= mobile
+            paddingTop: "100px",
+        },
+        [theme.breakpoints.up('md')]:{ // >= mobile
+            paddingTop: "0",
+        }
     }))
 
     const StyledImg = styled("img")(()=>({
-        width: "80%",
+        width: "75%",
         borderRadius: "50%",
         border: `1px solid ${theme.palette.primary.contrastText}`
     }))
@@ -36,16 +42,16 @@ const Hero = () => {
                         </Box>
                     </Grid>
                     <Grid item xs={12} md={7}>
-                        <Typography color="primary.contrastText" variant="h1" textAlign="center">Gabriel Duarte</Typography>
-                        <Grid container display="flex" justifyContent="center" spacing={3}>
+                        <Typography color="primary.contrastText" variant="h1" textAlign="center" pb="2">Gabriel Duarte</Typography>
+                        <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log ("download")}>
                                 <ArrowCircleDownIcon />
                                     <Typography>Download CV</Typography>
                                 </StyledButton>
                             </Grid>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={() => console.log("contato")}>
                                 <EmailIcon />
                                 <Typography>Contato</Typography>
                                 </StyledButton>
